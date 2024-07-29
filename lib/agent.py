af251c54855da19fddc50f9b3cc600f7dcf4f0a9
Moikapy <warrenckhan@gmail.com>
2024-07-28 22:58:28 -0400

    implement message queue for multiple users, and list ollama models with pull of llama3.1 latest if not found

--- lib/agent.py
+++ lib/agent.py
@@ -2,9 +2,18 @@
 from lib.llm import useOllama
 from langchain_core.messages import AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers.openai_functions import JsonOutputFunctionsParser
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.runnables import (
+    Runnable,
+    RunnableLambda,
+    RunnableMap,
+    RunnablePassthrough,
+)
 
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
 
 def agent_node(
     model="llama3.1:latest",
@@ -19,15 +28,23 @@ def agent_node(
 ):
     prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", system_message),
+            ("system", system_message +
+             "You have access to the following tools: {tool_names}.  I will avoid generateing fictional references. if tool_calling doesn't provide an answer I will try to find it on the web."),
             MessagesPlaceholder("memory"),
             ("human", "{content}"),
+            
         ]
     )
+
+    prompt = prompt.partial(tool_names=", ".join(
+        [tool.name for tool in tools]))
+
     ollama = useOllama(model=model, temperature=temperature)
     if tools:
-        ollama.bind_tools(tools)
-    chain = prompt | ollama
+        ollama = ollama.bind_tools(tools)
+
+    
+    chain = prompt | ollama | StrOutputParser() 
     if memory:
         chain = RunnableWithMessageHistory(
             chain,
